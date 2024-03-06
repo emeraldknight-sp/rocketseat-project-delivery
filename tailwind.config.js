54e2b1322d7459fc38c9e0e9db55b55d968f2ae1
David <contato.davidalmeida@outlook.com>
2024-03-06 15:16:37 -0300

    🔧 Add or update configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: "Inter_600SemiBold",
+        subtitle: "Inter_500Medium",
+        body: "Inter_400Regular",
+        bold: "Inter_700Bold",
+      },
+    },
+  },
+  plugins: [],
+};
