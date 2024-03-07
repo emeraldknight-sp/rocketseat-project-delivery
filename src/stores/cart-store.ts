ce9e8b2f64f00d445ed2cd46b88882c2668d83d5
David <contato.davidalmeida@outlook.com>
2024-03-07 20:08:36 -0300

    ✅ Add: Store files

---
+++ src/stores/cart-store.ts
@@ -0,0 +1,35 @@
+import * as cartInMemory from "./helpers/cart-in-memory";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ProductProps } from "@/utils/data/products";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+export interface ProductCartProps extends ProductProps {
+  quantity: number;
+}
+
+interface StateProps {
+  products: ProductCartProps[];
+  add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
+}
+
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+    }),
+    {
+      name: "rocketseat-project-delivery:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
